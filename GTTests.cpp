5b0ff361484a118b4a4b3e6eaec52b164787d20f
Hikaru5 <hikarukun5@gmail.com>
2019-04-25 17:04:17 -0700

    Add files via upload

---
+++ GTTests.cpp
@@ -0,0 +1,11 @@
+// GTTests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <gtest/gtest.h>
+
+TEST(GTTests, AssertTrue)
+{
+	ASSERT_TRUE(true);
+}
